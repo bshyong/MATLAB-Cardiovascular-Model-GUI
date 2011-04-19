eaaaec472cb7090787a27ac5208c3d5757e6960d
Ben Shyong <likeachild@gmail.com>
2011-04-19 11:08:29 -0400

    added pressure plots for chambers

--- final_gui.m
+++ final_gui.m
@@ -107,6 +107,77 @@ global data_table;
 data_table = handles.uitable1;
 set(data_table, 'Data', magic(8));
 
+%initialize for chamber plots
+         Iatrialres_L=(Yout(:,21)-Yout(:,14))/Ratrialres_L;
+         Iatrialres_R=(Yout(:,22)-Yout(:,14))/Ratrialres_R;
+         Iin_LA=(Iatrialres_L+Iatrialres_R); 
+         Iout_LA=max(((Yout(:,14)-Yout(:,15))/Rmv), 0);
+         I_LA=Iin_LA-Iout_LA;
+
+         %Left Ventricle
+         Iin_LV=Iout_LA;
+         Iout_LV=max((Yout(:,15)-Yout(:,6))/Rav, 0);
+         I_LV=Iin_LV-Iout_LV;
+
+         %Right Atrium
+         Iin_RA=Yout(:,3);
+         Iout_RA=max((Yout(:,16)-Yout(:,17))/Rtriv,0);
+         I_RA=Iin_RA-Iout_RA;
+
+         %Right Ventricle
+         Iin_RV=Iout_RA;
+         Iout_RV=Yout(:,23); 
+         I_RV=Iin_RV-Iout_RV;
+
+         %After 20 beats, choose 1 heartbeat (defined based on heart rate)
+         T=60/70;
+         A=find(Tout>(20*T)&Tout<(21*T));
+         Tsmall=(Tout(A)-Tout(A(1)));
+
+         Icut_LA=I_LA(A);
+         Icut_LV=I_LV(A);
+         Icut_RA=I_RA(A);
+         Icut_RV=I_RV(A);
+
+         for i=2:length(Tsmall)
+             Volume_LA(1)=0;
+             Volume_LV(1)=0;
+             Volume_RA(1)=0;
+             Volume_RV(1)=0;
+
+             Volume_LA(i)=trapz(Tsmall(1:i), Icut_LA(1:i));
+             Volume_LV(i)=trapz(Tsmall(1:i), Icut_LV(1:i));
+             Volume_RA(i)=trapz(Tsmall(1:i), Icut_RA(1:i));
+             Volume_RV(i)=trapz(Tsmall(1:i), Icut_RV(1:i));
+         end
+
+         %Add initial volumes
+         Volume_LA=Volume_LA+40;
+         Volume_LV=Volume_LV+120;
+         Volume_RA=Volume_RA;
+         Volume_RV=Volume_RV;
+
+         Pressure_LA=Yout(A,14);
+         Pressure_LV=Yout(A,15);
+         Pressure_RA=Yout(A,16);
+         Pressure_RV=Yout(A,17);
+
+axes(handles.axes3);
+ plot(Volume_LA,Pressure_LA, 'b.-')
+ title('Left Atrium');
+ 
+axes(handles.axes4);
+ plot(Volume_LV,Pressure_LV, 'b.-')
+ title('Left Ventricle');
+
+axes(handles.axes5);
+ plot(Volume_RA,Pressure_RA, 'b.-')
+ title('Right Atrium');
+ 
+axes(handles.axes6);
+ plot(Volume_RV,Pressure_RV, 'b.-')
+ title('Right Ventricle');
+         
 % update GUI
 guidata(hObject, handles);
 
